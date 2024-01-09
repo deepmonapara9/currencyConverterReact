6ea70a79e4d625975280c36982d29057079d4b43
deepmonapara9 <deepmonapara0.com>
2024-01-10 02:00:34 +0530

    intial commit

---
+++ src/components/inputBox.jsx
@@ -0,0 +1,55 @@
+import { React, useId } from "react";
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
+
+  const amountInputId = useId();
+
+  return (
+    <div className={`bg-slate-100 p-3 rounded-lg text-sm flex ${className} justify-center items-center`}>
+      <div className="w-1/2">
+        <label htmlFor={amountInputId} className="text-black mb-2 inline-block">
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className="outline-none bg-zinc-900 w-full rounded-md py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      //for currency type
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-zinc-900 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
